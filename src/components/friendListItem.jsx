71936c55266113920beeb9af988dc86c3da4e8ed
MrKyrylo <138040487+MrKyrylo@users.noreply.github.com>
2024-06-23 12:54:12 +0300

    1

--- src/components/friendListItem.jsx
+++ src/components/friendListItem.jsx
@@ -1,4 +1,4 @@
-import './friendListItem.module.css';
+import './src/components/friendListItem.module.css';
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   let status = isOnline ? 'online' : 'offline';
